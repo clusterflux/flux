9bd8c7b76be5b947eefa184378a9810b935d6408
shasha <clusterfluxx@gmail.com>
2013-01-04 03:52:48 -0800

    added hardcoded stone collision

--- WandW/src/com/example/wandw/MapView.java
+++ WandW/src/com/example/wandw/MapView.java
@@ -34,8 +34,8 @@ public class MapView extends View {
 	private int tile_height = 50;
 	public int screen_width = 12;
 	public int screen_height = 6;
-	public int playerX = 4;
-	public int playerY = 7;
+	public int playerX = 100;
+	public int playerY = 100;
 	public int centerTileX = screen_height/2 - 1;
 	public int centerTileY = screen_width/2 - 1;
 
@@ -47,37 +47,33 @@ public class MapView extends View {
 		Log.d("LOGCAT", "MapView created");	
 		
 		sprite = BitmapFactory.decodeResource(context.getResources(), R.drawable.sprite);
-		//overlayBitmap = Bitmap.createBitmap(screen_width*tile_width, screen_height*tile_height);
 		
 	}
 	
 	@Override
 	public void onDraw(Canvas canvas) {
 		
-		//canvas = new Canvas(overlayBitmap);
-		
 		//Draw tiles onto canvas
 		Log.d("LOGCAT", "drawing canvas");
 			
-		int screenX = -1;	// why?
+		int screenX = 0; //reset screenX each loop - this is where we will add an if statement to draw one column only
 		
-		for (int worldX = (playerX - screen_height/2); worldX < (playerX + screen_height/2 + 1); worldX += 1) {
-		
-			screenX += 1; 
-			int screenY = 0; //reset screenY each loop - this is where we will add an if statement to one column/row only
+		for (int worldX = (playerX - screen_height/2 + 1); worldX < (playerX + screen_height/2 + 1); worldX += 1, screenX += 1) {
 			
-			for(int worldY = (playerY - screen_width/2);  worldY < (playerY + screen_width/2 + 1); worldY += 1) {
+			int screenY = 0; //reset screenY each loop - this is where we will add an if statement to draw one row only
 			
-				//Log.d("LOGCAT", "Drawing tile @ = (" + screenX + ", " + screenY + 
-				//      ") [ world = (" + worldX + ", " + worldY + ") ]");
+			for (int worldY = (playerY - screen_width/2 + 1);  worldY < (playerY + screen_width/2 + 1); worldY += 1, screenY += 1) {
+				
+				if (screenX == 2 && screenY == 5) {
+				Log.d("LOGCAT", "Drawing tile @ = (" + screenX + ", " + screenY + ") [ world = (" + worldX + ", " + worldY + ") ]");
+				}
 			
 				canvas.drawBitmap(tileTranslator.get(world.world_map[worldX][worldY]), screenY*tile_height , screenX*tile_width, null);
-				//Log.d("LOGCAT", "World tile is" + world.world_map[worldX][worldY]); 
-				screenY += 1;
+
 			}
 		}
-		Log.d("LOGCAT", "Drawing sprite at " + (centerTileX*tile_height + tile_height/4) + "," + (centerTileY*tile_width + tile_width/4));
-		//draw sprite in center
+		
+		//draw sprite in center (2,5) on screen map
 		canvas.drawBitmap(sprite, centerTileY*tile_height + tile_height/5, centerTileX*tile_width + tile_width/5, null);
 
 		Log.d("LOGCAT", "done drawing canvas");	
@@ -93,25 +89,36 @@ public class MapView extends View {
 	}
 	
 	public void movePlayer(int movePlayerX, int movePlayerY) {
-	Log.d("LOGCAT", "Moving Player (" + movePlayerX + "," + movePlayerY + ")");
-		
-		
-		//check if player's movement will take him out of bounds
-		if ( playerY + movePlayerY < (world.world_width - screen_width/2 - 1) && playerX + movePlayerX < (world.world_height - screen_height/2 - 1) 
-			 && playerY + movePlayerY > screen_width/2 && playerX + movePlayerX > screen_height/2) {
 	
-			this.playerX += movePlayerX;
-			this.playerY += movePlayerY;
+	Log.d("LOGCAT", "Player @  (" + playerX + "," + playerY + ")");
+	Log.d("LOGCAT", "Player -> (" + movePlayerX + "," + movePlayerY + ")");
+
+		int newX = playerX + movePlayerX;
+        int newY = playerY + movePlayerY;
+
+		if (world.world_map[newX][newY] == 2) { //check if tile to move to is stone
+        
+            //do nothing
+            Log.d("LOGCAT", "Trying to cross stone. Cancel move");
+            
+		} else { //check if player's movement will take him out of bounds
+
+			if ( newY < (world.world_width - screen_width/2 - 1) && newX < (world.world_height - screen_height/2 - 1) 
+				&& newY > screen_width/2 && newX > screen_height/2) {
+	
+				this.playerX += movePlayerX;
+				this.playerY += movePlayerY;
 			
-			invalidate();
+				invalidate();
 			
-		} else {
+			} else {
 		
 			//do nothing
 			Toast.makeText(context, "REACHED THE END OF THE WORLD", Toast.LENGTH_SHORT).show();
 			
-		}
+			}
 			
+		}
 	}
 	
 }
