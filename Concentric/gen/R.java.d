d67d43a0efe36c1b2ba221dfdc79e19a72bf137d
shasha <clusterfluxx@gmail.com>
2013-01-19 22:32:37 -0800

    test2

--- Concentric/gen/R.java.d
+++ Concentric/gen/R.java.d
@@ -15,14 +15,4 @@ C:\Users\Shasha\Documents\GitHub\flux\Concentric\res\layout\worldgeneration.xml 
 C:\Users\Shasha\Documents\GitHub\flux\Concentric\res\layout\worldselection.xml \
 C:\Users\Shasha\Documents\GitHub\flux\Concentric\res\values\color.xml \
 C:\Users\Shasha\Documents\GitHub\flux\Concentric\res\values\strings.xml \
-C:\Users\Shasha\Documents\GitHub\flux\Concentric\bin\res\drawable-hdpi\dirt.png \
-C:\Users\Shasha\Documents\GitHub\flux\Concentric\bin\res\drawable-hdpi\game_map.png \
-C:\Users\Shasha\Documents\GitHub\flux\Concentric\bin\res\drawable-hdpi\grass.png \
-C:\Users\Shasha\Documents\GitHub\flux\Concentric\bin\res\drawable-hdpi\ic_launcher.png \
-C:\Users\Shasha\Documents\GitHub\flux\Concentric\bin\res\drawable-hdpi\sprite.png \
-C:\Users\Shasha\Documents\GitHub\flux\Concentric\bin\res\drawable-hdpi\stone.png \
-C:\Users\Shasha\Documents\GitHub\flux\Concentric\bin\res\drawable-hdpi\water.png \
-C:\Users\Shasha\Documents\GitHub\flux\Concentric\bin\res\drawable-ldpi\ic_launcher.png \
-C:\Users\Shasha\Documents\GitHub\flux\Concentric\bin\res\drawable-mdpi\ic_launcher.png \
-C:\Users\Shasha\Documents\GitHub\flux\Concentric\bin\res\drawable-xhdpi\ic_launcher.png \
 C:\Users\Shasha\Documents\GitHub\flux\Concentric\bin\AndroidManifest.xml \
